a822f0433d076c5593f1648948af31f423aab0d3
ScottE <scotteratigan@gmail.com>
2018-11-02 17:23:19 -0700

    Schedule now working properly.

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -18,9 +18,6 @@ function addNewTrain() {
     train.frequency = $('#train-frequency').val().trim();
     console.log($('#train-first-arrival').val().trim());
     train.firstArrival = $('#train-first-arrival').val().trim();
-    //train.firstArrival = moment($('#train-first-arrival').val().trim(), "HH:mm").unix();
-    // console.log(train.firstArrivalMoment);
-    // train.firstArrivalSeconds = train.firstArrivalMoment.unix();
     console.log(train.firstArrival);
     // todo: highlight required field of omitted?
     if (train.name && train.destination && train.frequency && train.firstArrival) {
@@ -37,57 +34,34 @@ function returnMinutes(moment) {
     return moment.hours() * 60 + moment.minutes();
 }
 
+function minutesToMoment(minutesInput) {
+    let hours = Math.floor(minutesInput / 60);
+    let minutes = minutesInput - (hours * 60);
+    return moment(`${hours}:${minutes}`, 'hh:mm A');
+}
+
 db.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
 
     // Store everything into a variable.
     let trainName = childSnapshot.val().name;
     let trainDestination = childSnapshot.val().destination;
-    let trainFrequency = childSnapshot.val().frequency; // in minutes
-    let firstArrival = moment(childSnapshot.val().firstArrival, "HH:mm"); // as a moment
-    console.log("First train arrival", firstArrival);
+    let trainFrequency = parseInt(childSnapshot.val().frequency); // in minutes
+    let firstArrival = moment(childSnapshot.val().firstArrival, "hh:mm"); // as a moment
     let firstArrivalMinutes = returnMinutes(firstArrival);
-    console.log("First train arrival (minutes)", firstArrivalMinutes);
     let currentTimeMinutes = returnMinutes(moment());
-    console.log('Current time minutes', currentTimeMinutes);
-    console.log('current time', moment().format('H:mm A'));
     let trainsPassed = Math.floor((currentTimeMinutes - firstArrivalMinutes) / trainFrequency);
-    console.log('trains passed', trainsPassed);
-    let minutesAway = (currentTimeMinutes - firstArrivalMinutes) % trainFrequency;
-    console.log('minutesAway', minutesAway);
-    let nextTrainMinutes = returnMinutes(moment()) + minutesAway;
-    console.log('nextTrainMinutes', nextTrainMinutes);
-    let nextTrainMoment = moment().set({hour:0,minute:0,second:0,millisecond:0});
-    nextTrainMoment.add(nextTrainMinutes, 'minutes');
-    let nextTrainTime = nextTrainMoment.format('h:mm A');
-    console.log('nextTrainTime', nextTrainTime);
-
-    //let timeSinceFirstTrain = firstArrival.diff(moment());
-    //console.log('Time since first train', timeSinceFirstTrain);
-    
-    
-    //let trainsPassed = Math.floor(firstArrivalMinutes / trainFrequency);
-    //console.log("Trains passed:", trainsPassed);
-    //let nextArrivalMinutes = firstArrivalMinutes + (trainsPassed * trainFrequency);
-    //console.log("Next arrival minutes", nextArrivalMinutes)
-    //let nextTrainTime = moment().minutes(nextArrivalMinutes).format('H:mm A');
-
-    // let minutesAway = firstArrivalMinutes % trainFrequency;
-    // console.log('Minutes away', minutesAway);
-    // let nextTrainMinutes = moment().minutes() + moment().hours() * 60 + minutesAway;
-    // console.log('Next train minutes', nextTrainMinutes);
-    // let nextTrainTime = moment().minutes(nextTrainMinutes).format('H:mm A');
-    // console.log('Next train time', nextTrainTime);
-    
-
-
+    let nextTrainMinutes = (trainsPassed * trainFrequency) + firstArrivalMinutes + trainFrequency;
+    let nextTrainTime = minutesToMoment(nextTrainMinutes);
+    let nextTrainDisplay = nextTrainTime.format('hh:mm A');
+    let minutesAway = nextTrainTime.diff(moment(), 'minutes');
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDestination),
         $("<td>").text(trainFrequency),
-        $('<td>').text(nextTrainTime),
+        $('<td>').text(nextTrainDisplay),
         $('<td>').text(minutesAway)
         );
     $('#list-of-trains').append(newRow);
